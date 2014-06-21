11dcafb1a28a59213631b0ad42f1436d074ab2d0
Matt Dobson <mdobson4@gmail.com>
2014-06-21 10:56:22 -0400

    Added resource implementations

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var argo = require('argo');
-var resource = require('argo-resource');
+var path = require('path');
+var titan = require('titan');
+var siren = require('argo-formatter-siren');
 var loaders = require('./loaders');
+var dbOpts = {
+  endpoint: process.env.LOCAL_HOST,
+  port: process.env.LOCAL_PORT,
+  password: "",
+  username: process.env.LOCAL_USER,
+  database: process.env.LOCAL_DB
+};
 
 var KeplerServer = module.exports = function() {
-  this.server = argo();
+  var self = this;
+  this.server = titan();
+  this.db = new loaders.PG(opts);
+  this.server.use(function(handle) {
+    handle('request', function(env, next) {
+      env.db = self.db;
+      next(env);
+    });
+  });
+  this.server.allow('*');
+  this.server.compress();
+  this.server.logger();
+  this.server.format({ engines: [ siren ], override: { 'application/json': siren } });
+  this.server.load(path.join(__dirname, 'resources'));
 };
 
 KeplerServer.prototype.listen = function() {
