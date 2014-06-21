a7a2439b7ddcaa989efb3f2d9fb3de34c9ba71d2
Matt Dobson <mdobson4@gmail.com>
2014-06-20 22:49:33 -0400

    Adding a hook into postgres. Starting basic server implementation.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+var argo = require('argo');
+var resource = require('argo-resource');
+var loaders = require('./loaders');
+
+var KeplerServer = module.exports = function() {
+  this.server = argo();
+};
+
+KeplerServer.prototype.listen = function() {
+  this.server.listen.apply(this.server, arguments);
+};
+
+KeplerServer.prototype.use = function() {
+  this.server.use.apply(this.server, arguments);
+};
 
